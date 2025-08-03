5a40bdc375edae3feeb2a7212ce40e0f69f12ce2
Lank <109240718+lankv2@users.noreply.github.com>
2025-08-03 07:27:16 -0700

    Add files via upload

---
+++ games/psu-clementine/amx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Moving to game directory
+cd drive_c/'Program Files (x86)'/'Phantasy Star Universe Clementine'
+
+# Run AntiMicroX
+./AntiMicroX-x86_64.AppImage --hidden --no-tray --profile psuc.gamecontroller.amgp
