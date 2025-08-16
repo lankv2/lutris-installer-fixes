9b8454755efbf768f97a0a218db2d9fe3bb7c8e5
Lank <109240718+lankv2@users.noreply.github.com>
2025-08-15 17:07:03 -0700

    Update amx.sh

--- games/psu-clementine/amx.sh
+++ games/psu-clementine/amx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Moving to game directory
-cd drive_c/'Program Files (x86)'/'Phantasy Star Universe Clementine'
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd
 
 # Run AntiMicroX
 ./AntiMicroX-x86_64.AppImage --hidden --no-tray --profile psuc.gamecontroller.amgp
